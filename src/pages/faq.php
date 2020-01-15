dc0c466e1de1e5d850f563f2e58e208021a96a3e
Sebastian Kersten <sebastiankersten@gmail.com>
2020-01-15 23:19:10 +0100

    Typo fix :pencil2:

--- src/pages/faq.php
+++ src/pages/faq.php
@@ -6,7 +6,7 @@
     <h1 class="faq_title">Frequently Asked Question</h1>
 
     <h2 class="faq_question">Why does this tool exist and what do I use it for?</h2>
-    <p class="faq_answer">I needed a tool that could help me when I want to login to some website, service or app on a public or a a friend's computer, while my passwords are in the password manager on my phone (and they are way to difficult to remember, which is a good thing; please use apps like 1Password or LastPass to improve the security of your data!)</p>
+    <p class="faq_answer">I needed a tool that could help me when I want to login to some website, service or app on a public or a friend's computer, while my passwords are in the password manager on my phone (and they are way to difficult to remember, which is a good thing; please use apps like 1Password or LastPass to improve the security of your data!)</p>
     <p class="faq_answer">If you don't have a service like Airdrop or your devices aren't all connected to the same iCloud account, sharing data between devices can be a hassle. Most of the time you are left with painstakingly typing in your password character by character while reading it from you phone. And having to at least start over three to five times because of a typo. Even more when your add the stress of colleagues or friends looking over your shoulder.</p>
     <p class="faq_answer">CopyPaste.me helps you transferring any password, texts or file from one device to another. So no more emailing yourself passwords or other things to get stuff from you phone to your computer. (don't judge :p, I think many of us have done it one or twice)</p>
 
