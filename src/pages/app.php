4924d9683e34f53e80c007c21df60185637ff69a
Sebastian Kersten <sebastiankersten@gmail.com>
2020-04-04 10:57:52 +0200

    Updated OG-image

--- src/pages/app.php
+++ src/pages/app.php
@@ -14,7 +14,7 @@
                 <div class="component_QR_card_sublabel">to manually connect another device</div>
                 <div data-mimoto-id="component_QR_manualurl" class="component_QR_manualurl show">
                     <div class="component_QR_manualurl_inner">
-                        <div class="component_QR_manualurl_url"><span data-mimoto-id="connect_url">https://copypaste.me</span>/connect</div>
+                        <div class="component_QR_manualurl_url"><a href="/connect"><span data-mimoto-id="connect_url">https://copypaste.me</span>/connect</a></div>
                         <div class="component_QR_manualurl_guidance">and enter<br>the following code:</div>
                         <div data-mimoto-id="manualcode" class="component_QR_manualurl_code"></div>
                         <div data-mimoto-id="countdown" class="component_QR_manualurl_validity">(valid for 2:40 minutes)</div>
